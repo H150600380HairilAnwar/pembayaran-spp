1f7ea9bf296e36a11590376102294f7dc0adf320
acer\pc <gusmanpoltanesa2016@gmail.com>
2016-12-14 08:32:13 +0800

    menambahkan kodingan pada button edit

--- Pembayaran SPP/Tabel Siswa.vb
+++ Pembayaran SPP/Tabel Siswa.vb
@@ -22,4 +22,19 @@
 
         SiswaBindingSource.AddNew()
     End Sub
+
+    Private Sub ButtonEdit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonEdit.Click
+        NISTextBox.Enabled = True
+        NamaTextBox.Enabled = True
+        KelasTextBox.Enabled = True
+        No_AbsenTextBox.Enabled = True
+        JurusanTextBox.Enabled = True
+
+        ButtonTambah.Enabled = False
+        ButtonEdit.Enabled = False
+        ButtonHapus.Enabled = False
+        ButtonRefresh.Enabled = False
+        ButtonBatal.Enabled = True
+        ButtonSimpan.Enabled = True
+    End Sub
 End Class
